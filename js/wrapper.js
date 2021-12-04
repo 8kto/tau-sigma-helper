fed9ea4485bedcd318c01c2d39994d8a9e683686
Okto <web@axisful.info>
2021-12-04 13:05:51 +0100

    Render all samples playable

--- js/wrapper.js
+++ js/wrapper.js
@@ -1,21 +1,38 @@
 (() => {
   const speechBase = './speech'
   const speech = [
-    'Вас приветствует Универсальный гео-разведывательный.wav',
-    'Введите запрос.wav',
-    'Введите запрос или выберите из пунктов меню.wav',
-    'Выполняю запрос.wav',
-    'Документ1 1.wav',
-    'Документ1 2.wav',
-    'Документ1 3.wav',
-    'Документ1 4.wav',
-    'Слава Союзу!.wav',
-    'Товарищи!.wav',
-    'Хая Соломоновна, вы не против сегодня поужинать вместе.wav',
+    'Слава Союзу! X.mp3',
+    'Вас приветствует Универсальный гео-разведывательный X.mp3',
+    'Введите запрос или выберите из пунктов меню X.mp3',
+    '1 Вложить образец породы X.mp3',
+    '2 Связаться со станцией X.mp3',
+    '3 Получить карту местности X.mp3',
+    '4 Прослушать анекдот X.mp3',
+    'Введите запрос X.mp3',
+    'Выполняю запрос X.mp3',
+    'Анек glitch X.mp3',
+    'Анек полный X.mp3',
+    'Товарищи! X.mp3',
   ]
+  const template = `
+    <div class="p-3 mb-4 mr-2">
+      <p class="sample-title text-truncate font-weight-bold">{title}</p>
+      <audio controls preload="auto">
+        <source src="{src}" type="audio/mpeg">
+      </audio>
+    </div>
+    `
+
+  const getTitle = src => src.replace('X.mp3', '')
 
   jQuery(() => {
-    console.log(speech)
+    $('#speech-container').html(
+      speech.map(path => {
+        return template
+          .replace('{src}', `${speechBase}/${path}`)
+          .replace('{title}', getTitle(path))
+      })
+    )
   })
 
 })()
